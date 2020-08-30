992b3a7a8e641a3f8b76782b7912afa32bf80296
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2020-08-30 18:40:53 +0530

    🔧 Small Fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
 
 import React from "react";
-import Sections from "./sections/sections.jsx";
+import NavBar from "./components/navBar.jsx";
+import StorySection from "./components/storySection.jsx";
+import InitiativesSection from "./components/initiativesSection.jsx";
+import EventsSection from "./components/eventsSection.jsx";
+import BlogsSection from "./components/blogsSection.jsx";
+import BoardMembersSection from "./components/boardmembersSection.jsx";
+import GetInTouchSection from "./components/getInTouchSection.jsx";
+import AboutUsSecton from "./components/aboutUsSection.jsx";
+import Footer from "./components/footer.jsx";
 
 function App() {
-  return <Sections />;
+  return (
+    <React.Fragment>
+      <NavBar />
+      <StorySection />
+      <InitiativesSection />
+      <EventsSection />
+      <BlogsSection />
+      <BoardMembersSection />
+      <GetInTouchSection />
+      <AboutUsSecton />
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default App;
